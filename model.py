4763071faceebfce62dc4159ceb714eb5a986eaa
syedareehaquasar <syedareehaquasar@mail.com>
2020-06-26 01:49:08 +0530

    importing files

---
+++ model.py
@@ -0,0 +1,10 @@
+#loading libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matpltlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import 
+from sklearn.metrics import
